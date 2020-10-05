39d2f28e3dd13751709efe49f6d7e3a1ff80b0fd
Rashi Kulshrestha <rashikul5.rk@gmail.com>
2020-10-05 22:29:54 +0530

    Update NavBar.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React, { Component, Fragment } from "react";
+
+class NavBar extends Component{
+    render(){
+        return(
+            <Fragment>
+                <nav className="nav-dark">
+                    <div>
+                        <span><i className="fas fa-book"/></span>
+                        <span>Courseplus</span>
+                    </div>
+                    <div className="searchbar">
+                        <input placeholder="Search..."></input><span className="icon"><i className="fas fa-search"/></span>
+                    </div>
+                    <div className="utils">
+                        <span><i className="fas fa-bell"/></span>
+                        <span><i class="fa fa-envelope" aria-hidden="true"></i></span>
+                        <span><i class="fa fa-user-circle-o"></i></span> Richard Ali
+                    </div>
+                </nav>
+            </Fragment>
+        )
+    }
+    
+}
+export default NavBar;
